c18a1c5399e5292fe8487cf41d6779156d653c84
Ratokki <itokkiralaiarivelo@gmail.com>
2025-12-11 16:18:39 +0300

    modif1 commit - Projet Jenkins avec Maven

--- src/main/java/com/projetjenkins/Main.java
+++ src/main/java/com/projetjenkins/Main.java
@@ -7,7 +7,7 @@ public class Main {
         // Message modifié pour déclencher le build automatique #2 dans Jenkins
         System.out.println("Hello Jenkins – Build automatique réussi !");
 
-        System.out.println("Petite demonstration");
+        System.out.println("Démonstration pour le test automatique");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
